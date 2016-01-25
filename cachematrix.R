da135a4f38caa7637ab5f3dcaeaae6d6744b21b4
mlromerorod <rrodrigm@reduno.com.mx>
2016-01-25 01:15:21 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+Matrizinversa  <- NULL
+        set <- function(y) {
+                x <<- y
+               Matrizinversa  <<- NULL
+        }
+        get <- function() x
+        setMatrizinversa <- function(solve) Matrizinversa <<- solve
+        getMatrizinversa <- function()Matrizinversa
+        list(set = set, get = get,
+             setMatrizinversa = setMatrizinversa,
+             getMatrizinversa = getMatrizinversa)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+               Matrizinversa <- x$getMatrizinversa ()
+        if(!is.null(Matrizinversa)) {
+                message("getting cached data")
+                return(Matrizinversa)
+        }
+        data <- x$get()
+       Matrizinversa<- solve(data, ...)
+        x$setMatrizinversa (Matrizinversa)
+       Matrizinversa
 }
